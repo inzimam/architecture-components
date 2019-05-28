92b1983e2634e4d60f9fee1697fb4bb0adc51c9a
firoz.anawar <firoz.anawar@timesinternet.in>
2019-05-28 23:38:22 +0530

    db helper basic added

---
+++ app/src/main/java/com/inzy/practice/db/FavouritesDBHelper.java
@@ -0,0 +1,22 @@
+package com.inzy.practice.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class FavouritesDBHelper extends SQLiteOpenHelper {
+
+    public FavouritesDBHelper(Context context) {
+        super(context, DbSettings.DB_NAME, null, DbSettings.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createtable =
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
